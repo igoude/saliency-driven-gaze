5292ac2ee56565dbed962bce32d66c9804e90acf
igoude <goude.ific@gmail.com>
2025-01-03 12:41:09 -0500

    Remove project 0.0.1

--- Unreal Project/Source/Saliency_driven_gaze/Saliency_driven_gaze.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Saliency_driven_gaze.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Saliency_driven_gaze, "Saliency_driven_gaze" );
